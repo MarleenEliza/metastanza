ad680800afb036b8027c7ad69822a7bf2e41b498
Yoji Shidara <dara@shidara.net>
2021-04-22 10:54:29 +0900

    Update Hash table stanza to be class-based

--- stanzas/hash-table/index.js
+++ stanzas/hash-table/index.js
@@ -1,44 +1,50 @@
 import loadData from "@/lib/load-data";
+import Stanza from "togostanza/stanza";
 
-export default async function hashTable(stanza, params) {
-  let dataset = await loadData(params["data-url"], params["data-type"]);
-  dataset = dataset[0];
+export default class extends Stanza {
+  async render() {
+    console.log("HI");
 
-  const columns = params.columns
-    ? JSON.parse(params.columns)
-    : Object.keys(dataset).map((key) => {
-        return { id: key };
+    const params = this.params;
+
+    let dataset = await loadData(params["data-url"], params["data-type"]);
+    dataset = dataset[0];
+
+    const columns = params.columns
+      ? JSON.parse(params.columns)
+      : Object.keys(dataset).map((key) => {
+          return { id: key };
+        });
+    const values = columns.map((column) => {
+      const datum_label = Object.keys(dataset).find((datum) => {
+        return datum === column.id;
       });
-  const values = columns.map((column) => {
-    const datum_label = Object.keys(dataset).find((datum) => {
-      return datum === column.id;
+      const label = column.label
+        ? column.label
+        : params["format-key"] === "true"
+        ? datum_label.charAt(0).toUpperCase() +
+          datum_label.substring(1).replace(/_/g, " ")
+        : datum_label;
+      const href = column.link ? dataset[column.link] : null;
+      return {
+        label,
+        value: dataset[column.id],
+        href,
+        unescape: column.escape === false,
+      };
     });
-    const label = column.label
-      ? column.label
-      : params["format-key"] === "true"
-      ? datum_label.charAt(0).toUpperCase() +
-        datum_label.substring(1).replace(/_/g, " ")
-      : datum_label;
-    const href = column.link ? dataset[column.link] : null;
-    return {
-      label,
-      value: dataset[column.id],
-      href,
-      unescape: column.escape === false,
-    };
-  });
-  stanza.render({
-    template: "stanza.html.hbs",
-    parameters: {
-      values,
-    },
-  });
+    this.renderTemplate("stanza.html.hbs", {
+      parameters: {
+        values,
+      },
+    });
 
-  const main = stanza.root.querySelector("main");
-  main.setAttribute(
-    "style",
-    `width: ${params["width"]};
+    const main = this.root.querySelector("main");
+    main.setAttribute(
+      "style",
+      `width: ${params["width"]};
     height: ${params["height"]};
     padding: ${params["padding"]}`
-  );
+    );
+  }
 }
