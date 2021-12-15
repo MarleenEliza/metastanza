9f476929fcc0f19a7afef3f6eb2b96672b2c1876
teraomoyu <dmntc.5299@gmail.com>
2021-12-15 22:45:50 +0900

    change to responsive heatmap

--- stanzas/heatmap/index.js
+++ stanzas/heatmap/index.js
@@ -6,13 +6,23 @@ import * as d3 from "d3";
 export default class Heatmap extends Stanza {
   async render() {
     // if (typeof variants === "object") {
-    draw(this, this.params);
+      const css = (key) => getComputedStyle(this.element).getPropertyValue(key);
+      const chartElement = this.root.querySelector("main");
+      const params = {
+        top: this.params["top"],
+        right: this.params["right"],
+        bottom: this.params["bottom"],
+        left: this.params["left"],
+        width: this.params["width"],
+        height: this.params["height"],
+      };
+    draw(chartElement, css, params);
     // }
   }
 }
 
-async function draw(stanza, params) {
-  const css = (key) => getComputedStyle(stanza.element).getPropertyValue(key);
+async function draw(el, css, params) {
+  // const css = (key) => getComputedStyle(stanza.element).getPropertyValue(key);
   // const controlElement = stanza.root.querySelector("#control");
 
   // set the dimensions and margins of the graph
@@ -26,34 +36,22 @@ async function draw(stanza, params) {
     width = params["width"] - margin.left - margin.right,
     height = params["height"] - margin.top - margin.bottom;
 
-  const chartElement = stanza.root.querySelector("main");
-
+  // remove svg element when params updated
+  d3.select(el).select("svg").remove();
+    
   // append the svg object to the body of the page
   const svg = d3
-    .select(chartElement)
+    .select(el)
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  // const ctrlSvg = d3
-  //   .select(controlElement)
-  //   .append("svg");
-  // ctrlSvg
-  //   .append("rect")
-  //   .attr("fill", "var(--togostanza-series-1-color)")
-  //   .attr("stroke", "#99ACB2")
-
-  // const plotGroup = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
-
   // Labels of row and columns
-
   const myGroups = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
   const myVars = ["v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10"];
 
-  // plotGroup
-  //   .call(d3.axisBottom(x));
   // Build X scales and axis:
   const x = d3.scaleBand().range([0, width]).domain(myGroups).padding(0.01);
   svg
@@ -98,35 +96,35 @@ async function draw(stanza, params) {
       });
   });
 
-  reRender();
+  // reRender();
 
-  function reRender() {
-    const myColor = d3
-      .scaleLinear()
-      .range([
-        css("--togostanza-series-0-color"),
-        css("--togostanza-series-1-color"),
-      ])
-      .domain([1, 100]);
+  // function reRender() {
+  //   const myColor = d3
+  //     .scaleLinear()
+  //     .range([
+  //       css("--togostanza-series-0-color"),
+  //       css("--togostanza-series-1-color"),
+  //     ])
+  //     .domain([1, 100]);
 
-    dataset.then(function (data) {
-      svg
-        .selectAll()
-        .data(data, function (d) {
-          return d.group + ":" + d.variable;
-        })
-        .join("rect")
-        .attr("x", function (d) {
-          return x(d.group);
-        })
-        .attr("y", function (d) {
-          return y(d.variable);
-        })
-        .attr("width", x.bandwidth())
-        .attr("height", y.bandwidth())
-        .style("fill", function (d) {
-          return myColor(d.value);
-        });
-    });
-  }
+  //   dataset.then(function (data) {
+  //     svg
+  //       .selectAll()
+  //       .data(data, function (d) {
+  //         return d.group + ":" + d.variable;
+  //       })
+  //       .join("rect")
+  //       .attr("x", function (d) {
+  //         return x(d.group);
+  //       })
+  //       .attr("y", function (d) {
+  //         return y(d.variable);
+  //       })
+  //       .attr("width", x.bandwidth())
+  //       .attr("height", y.bandwidth())
+  //       .style("fill", function (d) {
+  //         return myColor(d.value);
+  //       });
+  //   });
+  // }
 }
